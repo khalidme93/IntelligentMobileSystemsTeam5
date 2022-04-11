0cc884d76371b13fde79eb7ae2451046d0e01f0e
HugoMartinsson <Hugo.Martinsson01@gmail.com>
2022-04-11 14:15:12 +0200

    Created LargeButton-component

--- mobile-app/src/src/components/Buttons/LargeButton.jsx
+++ mobile-app/src/src/components/Buttons/LargeButton.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+import colors from '../../constants/colors';
+
 
 const styles = StyleSheet.create({
-  container: {
-
-  }
+  button: {
+    width: 175,
+    height: 50,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: { //Change font
+    fontSize: 24,
+  },
 });
 
-export default function LargeButton() {
+
+
+export default function LargeButton({title, color}) {
+  const onPress = () => {
+    // TODO: Do navigation
+  }
+
+  var textColor = colors.SNOW;
+  if(color == colors.SNOW) {
+    textColor = colors.BLACK;
+  }
+
   return (
-    <View style={styles.container}>
+    <View>
+      <TouchableOpacity onPress={onPress} style={[styles.button, { backgroundColor: color }]}>
+        <Text style={{ color: textColor }}>{title}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
