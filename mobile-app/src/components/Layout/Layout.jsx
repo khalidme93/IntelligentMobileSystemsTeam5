ff1b3ca4685b00beee083afa5a3f5617437fce68
hugodezordo <hugo.de-zordo@epitech.eu>
2022-04-11 16:28:20 +0200

    layout

--- mobile-app/src/components/Layout/Layout.jsx
+++ mobile-app/src/components/Layout/Layout.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { View } from 'react-native';
+import Wave from './components/Wave';
 
-export default function Layout() {
-
+export default function Layout({ children }) {
+  return (
+    <View>
+      <Wave/>
+      {children}
+    </View>
+  );
 }
