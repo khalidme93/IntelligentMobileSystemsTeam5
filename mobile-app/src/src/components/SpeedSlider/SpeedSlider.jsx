343c14e70ac9f6c3c11af468398ab7da2898658b
HugoMartinsson <Hugo.Martinsson01@gmail.com>
2022-04-11 15:27:00 +0200

    Created SpeedSlider-component

---
+++ mobile-app/src/src/components/SpeedSlider/SpeedSlider.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet, Text, Dimensions } from 'react-native';
+import Slider from 'react-native-slider';
+import colors from '../../constants/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    width: Dimensions.get('window').width * 0.85,
+    height: 30,
+  },
+  slider: {
+    width: Dimensions.get('window').width * 0.85,
+    height: 30,
+    color: colors.PRIMARY,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
+
+export default function SpeedSlider({onSlidingComplete}) {
+  return (
+    <View style={styles.container}>
+      <Slider maximunValue={100} onSlidingComplete={onSlidingComplete} minimumTrackTintColor={colors.GRAY} maximumTrackTintColor={colors.GRAY} thumbTintColor={colors.PRIMARY_DARK}/>
+      <Text style={styles.text}>Speed</Text>
+    </View>
+  );
+};
